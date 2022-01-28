4150950036cb9b9c39b82ca9266df12b8faf3ac4
babyazalea <usun16@gmail.com>
2022-01-28 12:12:18 +0900

    added options component & buttons

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,10 +1,35 @@
-const { Component } = require("react");
+import { Component } from "react";
+
+import countriesData from "../../assets/countries.json";
+import categoriesData from "../../assets/categoies.json";
+
+import styles from "./Options.module.css";
 
 class Options extends Component {
+  getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split("")
+      .map((char) => 127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+  }
+
   render() {
     return (
-      <div className="options">
-        <p>옵션을 선택해보세요</p>
+      <div className={styles.options}>
+        <div className={styles["country-buttons"]}>
+          {countriesData.countries.map((country) => (
+            <button key={country.name}>
+              <span>{country["kor-name"]}</span>
+              <span>{this.getFlagEmoji(country.code)}</span>
+            </button>
+          ))}
+        </div>
+        <div className={styles["category-buttons"]}>
+          {categoriesData.categories.map((category) => (
+            <button key={category.name}>{category["kor-name"]}</button>
+          ))}
+        </div>
       </div>
     );
   }
