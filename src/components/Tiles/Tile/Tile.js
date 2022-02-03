5b1a0a667ec3ac1ce8a7af33755f7c94ca9dca42
babyazalea <usun16@gmail.com>
2022-02-03 22:21:40 +0900

    converted functional Tiles & Tile

--- src/components/Tiles/Tile/Tile.js
+++ src/components/Tiles/Tile/Tile.js
@@ -1,18 +1,14 @@
-import { Component } from "react";
-
 import styles from "./Tile.module.css";
 
-class Tile extends Component {
-  render() {
-    return (
-      <div className={styles.tile}>
-        <span className={styles["article-title"]}>
-          {this.props.article.title}
-        </span>
-        <a href={this.props.article.url}>기사 링크로 이동</a>
-      </div>
-    );
-  }
+function Tile() {
+  return (
+    <div className={styles.tile}>
+      <span className={styles["article-title"]}>
+        {this.props.article.title}
+      </span>
+      <a href={this.props.article.url}>기사 링크로 이동</a>
+    </div>
+  );
 }
 
 export default Tile;
--- src/components/Tiles/Tiles.js
+++ src/components/Tiles/Tiles.js
@@ -1,19 +1,15 @@
-import { Component } from "react";
-
 import Tile from "./Tile/Tile";
 
 import styles from "./Tiles.module.css";
 
-class Tiles extends Component {
-  render() {
-    return (
-      <div className={styles.tiles}>
-        {this.props.articles.map((article, index) => (
-          <Tile key={index} article={article} />
-        ))}
-      </div>
-    );
-  }
+function Tiles() {
+  return (
+    <div className={styles.tiles}>
+      {this.props.articles.map((article, index) => (
+        <Tile key={index} article={article} />
+      ))}
+    </div>
+  );
 }
 
 export default Tiles;
