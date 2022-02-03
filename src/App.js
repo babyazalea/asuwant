307d8b1faf6d40c9ac5f92402e1c22fb9629d777
babyazalea <usun16@gmail.com>
2022-02-03 21:21:54 +0900

    converted react router v6 & App, ErrorPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Component, Fragment } from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Fragment, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import axios from "axios";
 
 import Layout from "./components/UI/Layout";
@@ -11,25 +11,23 @@ import Tiles from "./components/Tiles/Tiles";
 
 import "./App.css";
 
-class App extends Component {
-  state = {
-    isLoading: false,
-    errorCode: null,
-    chosenCountry: null,
-    chosenCategory: null,
-    articles: [],
-  };
+function App() {
+  // state = {
+  //   isLoading: false,
+  //   errorCode: null,
+  //   chosenCountry: null,
+  //   chosenCategory: null,
+  //   articles: [],
+  // };
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorCode, setErrorCode] = useState(null);
+  const [chosenCountry, setChosenCountry] = useState(null);
+  const [chosenCategory, setChosenCategory] = useState(null);
+  const [articles, setArticles] = useState([]);
 
-  confirmedOptions = (country, category) => {
+  const confirmedOptions = (country, category) => {
     // start loading
-    this.setState((prevState) => {
-      return {
-        ...prevState,
-        isLoading: true,
-        chosenCountry: country,
-        chosenCategory: category,
-      };
-    });
+    setIsLoading(true);
 
     const url = process.env.REACT_APP_BACKEND_URL;
 
@@ -45,90 +43,69 @@ class App extends Component {
       .get(url, params)
       .then((res) => {
         if (res.status === 200) {
-          this.setState((prevState) => {
-            return {
-              ...prevState,
-              articles: res.data.articles,
-              isLoading: false,
-            };
-          });
+          setArticles(res.data.articles);
+          setIsLoading(false);
         }
       })
       .catch((error) => {
-        this.setState((prevState) => {
-          return {
-            ...prevState,
-            isLoading: false,
-            errorCode: error.response
-              ? error.response.data.code
-              : "Error Occurred",
-          };
-        });
+        setIsLoading(false);
+        setErrorCode(
+          error.response ? error.response.data.code : "Error Occurred"
+        );
       });
   };
 
-  resetApp = () => {
-    this.setState(() => {
-      return {
-        isLoading: false,
-        errorCode: null,
-        chosenCountry: null,
-        chosenCategory: null,
-        articles: [],
-      };
-    });
+  const resetApp = () => {
+    setIsLoading(false);
+    setErrorCode(null);
+    setChosenCountry(null);
+    setChosenCategory(null);
+    setArticles([]);
   };
 
-  routes = () => (
-    <Switch>
-      <Route path="/" exact>
-        {this.state.errorCode !== null ? (
-          <Redirect to="/error" />
-        ) : this.state.chosenCountry && this.state.chosenCategory ? (
-          <Fragment>
-            {this.state.isLoading ? (
-              <Loading />
-            ) : (
-              <Fragment>
-                {this.state.errorCode == null && (
-                  <Tiles articles={this.state.articles} />
-                )}
-              </Fragment>
-            )}
-          </Fragment>
-        ) : (
-          <Options confirmedOptions={this.confirmedOptions} />
-        )}
-      </Route>
-      <Route path="/credits">
-        <Credits />
-      </Route>
-      <Route path="/error">
-        <ErrorPage errorCode={this.state.errorCode} resetApp={this.resetApp} />
-      </Route>
-    </Switch>
+  const routes = (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          errorCode !== null ? (
+            <Navigate to="/error" />
+          ) : chosenCountry && chosenCategory ? (
+            <Fragment>
+              {isLoading ? (
+                <Loading />
+              ) : (
+                <Fragment>
+                  {errorCode == null && <Tiles articles={articles} />}
+                </Fragment>
+              )}
+            </Fragment>
+          ) : (
+            <Options confirmedOptions={confirmedOptions} />
+          )
+        }
+      ></Route>
+      <Route path="/credits" element={<Credits />} />
+      <Route
+        path="/error"
+        element={<ErrorPage errorCode={errorCode} resetApp={resetApp} />}
+      />
+    </Routes>
   );
 
-  componentDidUpdate() {
-    if (this.state.errorCode !== null) {
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Layout
-          isError={this.state.errorCode !== null}
-          isLoading={this.state.isLoading}
-          chosenCountry={this.state.chosenCountry}
-          chosenCategory={this.state.chosenCategory}
-          resetApp={this.resetApp}
-        >
-          {this.routes()}
-        </Layout>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Layout
+        isError={errorCode !== null}
+        isLoading={isLoading}
+        chosenCountry={chosenCountry}
+        chosenCategory={chosenCategory}
+        resetApp={resetApp}
+      >
+        {routes}
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Error/ErrorPage.js
+++ src/components/Error/ErrorPage.js
@@ -1,57 +1,63 @@
-import { Component, Fragment } from "react";
-import { NavLink, withRouter } from "react-router-dom";
+import { Fragment, useEffect, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 import styles from "./ErrorPage.module.css";
 
-class ErrorPage extends Component {
-  state = {
-    rateLimited: false,
-  };
+function ErrorPage(props) {
+  // state = {
+  //   rateLimited: false,
+  // };
 
-  componentDidMount() {
-    if (this.props.errorCode === "rateLimited") {
-      this.setState({
-        rateLimited: true,
-      });
+  // componentDidMount() {
+  //   if (this.props.errorCode === "rateLimited") {
+  //     this.setState({
+  //       rateLimited: true,
+  //     });
+  //   }
+
+  //   if (this.props.errorCode === null) {
+  //     this.props.history.push("/");
+  //   }
+  // }
+
+  const [errorCode, setErrorCode] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (props.errorCode !== null) {
+      setErrorCode(props.errorCode);
+      return;
     }
 
-    if (this.props.errorCode === null) {
-      this.props.history.push("/");
-    }
-  }
+    navigate("/");
+  }, [props.errorCode, navigate]);
 
-  errorMessage = () => {
-    if (this.state.rateLimited) {
-      return (
-        <Fragment>
-          <h1>News API 사용량이 제한되었습니다.</h1>
-          <p>
-            asuwant는 News API의 Free Tier를 사용하고 있어, 하루 사용량이
-            100회의 요청으로 제한되어 있습니다. 일정한 시간이 지난 후(최소 하루)
-            다시 시도해주세요.
-          </p>
-        </Fragment>
-      );
-    }
-
-    return (
+  const errorMessage =
+    errorCode === "rateLimited" ? (
+      <Fragment>
+        <h1>News API 사용량이 제한되었습니다.</h1>
+        <p>
+          asuwant는 News API의 Free Tier를 사용하고 있어, 하루 사용량이 100회의
+          요청으로 제한되어 있습니다. 일정한 시간이 지난 후(최소 하루) 다시
+          시도해주세요.
+        </p>
+      </Fragment>
+    ) : (
       <Fragment>
         <h1>알 수 없는 오류가 발생했습니다.</h1>
         <p>다시 시도해주세요.</p>
       </Fragment>
     );
-  };
 
-  render() {
-    return (
-      <div className={styles.error}>
-        {this.errorMessage()}
-        <NavLink to="/" onClick={() => this.props.resetApp()}>
-          다시 하기
-        </NavLink>
-      </div>
-    );
-  }
+  return (
+    <div className={styles.error}>
+      {errorMessage}
+      <NavLink to="/" onClick={() => props.resetApp()}>
+        다시 하기
+      </NavLink>
+    </div>
+  );
 }
 
-export default withRouter(ErrorPage);
+export default ErrorPage;
