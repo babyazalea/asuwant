f9fbd470984b2af99e5de96bd0729e2b326e86ab
babyazalea <usun16@gmail.com>
2022-02-03 18:46:53 +0900

    converted functional component

--- src/components/Credits/Credits.js
+++ src/components/Credits/Credits.js
@@ -1,43 +1,39 @@
-import { Component } from "react";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faCopyright } from "@fortawesome/free-regular-svg-icons";
 
 import styles from "./Credits.module.css";
 
-class Credits extends Component {
-  render() {
-    return (
-      <div className={styles.credits}>
-        <div className={styles["credits-wrapper"]}>
-          <p>asuwant는 영리적인 목적으로 만들어진 웹이 아닙니다.</p>
-          <div>
-            <p>
-              뉴스 데이터의 출처는 <a href="https://newsapi.org/">News API</a>,
-              로딩 페이지 일러스트레이션은{" "}
-              <a href="https://sophieharristaylor.com/">Sophie Harris Taylor</a>
-              의 사진에서 영감을 받았습니다.
-            </p>
-            <p>
-              <a
-                href="https://github.com/babyazalea/asuwant-frontend"
-                className={styles["github-link"]}
-              >
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-            </p>
-            <p className={styles.copyright}>
-              <span>
-                <FontAwesomeIcon icon={faCopyright} />
-                <a href="https://babyazalea.gatsbyjs.io/">babayazalea</a>
-              </span>
-            </p>
-          </div>
+function Credits() {
+  return (
+    <div className={styles.credits}>
+      <div className={styles["credits-wrapper"]}>
+        <p>asuwant는 영리적인 목적으로 만들어진 웹이 아닙니다.</p>
+        <div>
+          <p>
+            뉴스 데이터의 출처는 <a href="https://newsapi.org/">News API</a>,
+            로딩 페이지 일러스트레이션은{" "}
+            <a href="https://sophieharristaylor.com/">Sophie Harris Taylor</a>의
+            사진에서 영감을 받았습니다.
+          </p>
+          <p>
+            <a
+              href="https://github.com/babyazalea/asuwant-frontend"
+              className={styles["github-link"]}
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </p>
+          <p className={styles.copyright}>
+            <span>
+              <FontAwesomeIcon icon={faCopyright} />
+              <a href="https://babyazalea.gatsbyjs.io/">babayazalea</a>
+            </span>
+          </p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Credits;
