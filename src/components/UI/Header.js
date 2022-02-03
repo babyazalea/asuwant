1618ff3f86e98b87233d996f0723044e59241fe7
babyazalea <usun16@gmail.com>
2022-02-03 22:27:14 +0900

    converted functional Header

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,4 +1,5 @@
-import { Component, Fragment } from "react";
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -8,51 +9,46 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Header.module.css";
-import { Link } from "react-router-dom";
 
-class Header extends Component {
-  getFlagEmoji(countryCode) {
+function Header(props) {
+  const getFlagEmoji = (countryCode) => {
     const codePoints = countryCode
       .toUpperCase()
       .split("")
       .map((char) => 127397 + char.charCodeAt());
     return String.fromCodePoint(...codePoints);
-  }
+  };
 
-  navRight = () => {
-    if (!this.props.chosenCountry && !this.props.chosenCategory) {
-      return;
-    }
+  let navRight;
 
-    return (
+  if (props.chosenCountry && props.chosenCategory) {
+    navRight = (
       <div className={styles["nav-right"]}>
         <div className={styles.options}>
           <span className={styles["options-text-lg"]}>
             <span>
-              {this.props.chosenCountry["kor-name"]}의{" "}
-              {this.props.chosenCategory["kor-name"]} 뉴스
-              {this.props.isLoading ? " 불러오는 중..." : "를 보고 있습니다."}
+              {props.chosenCountry["kor-name"]}의{" "}
+              {props.chosenCategory["kor-name"]} 뉴스
+              {props.isLoading ? " 불러오는 중..." : "를 보고 있습니다."}
             </span>
           </span>
           <span className={styles["options-text-sm"]}>
-            {this.props.isLoading ? (
+            {props.isLoading ? (
               <span>
                 <FontAwesomeIcon icon={faSpinner} pulse />
               </span>
             ) : (
               <Fragment>
-                <span>
-                  {this.getFlagEmoji(this.props.chosenCountry["code"])}
-                </span>
-                <span>{this.props.chosenCategory["kor-name"]}</span>
+                <span>{getFlagEmoji(props.chosenCountry["code"])}</span>
+                <span>{props.chosenCategory["kor-name"]}</span>
               </Fragment>
             )}
           </span>
         </div>
-        {!this.props.isLoading && (
+        {!props.isLoading && (
           <button
             className={styles["reset-button"]}
-            onClick={() => this.props.resetApp()}
+            onClick={() => props.resetApp()}
           >
             <FontAwesomeIcon icon={faRedo} />
             <span className={styles["reset-button-text"]}>
@@ -62,30 +58,28 @@ class Header extends Component {
         )}
       </div>
     );
-  };
-
-  render() {
-    return (
-      <header className={styles.header}>
-        <nav className={styles.nav}>
-          <div className={styles["nav-left"]}>
-            <div className={styles.logo}>
-              <Link to="/">asuwant</Link>
-            </div>
-            <div className={styles.credits}>
-              <Link to="/credits">
-                <span className={styles["credits-text-lg"]}>credits</span>
-                <span className={styles["credits-text-sm"]}>
-                  <FontAwesomeIcon icon={faQuestionCircle} />
-                </span>
-              </Link>
-            </div>
-          </div>
-          {!this.props.isError && this.navRight()}
-        </nav>
-      </header>
-    );
   }
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles["nav-left"]}>
+          <div className={styles.logo}>
+            <Link to="/">asuwant</Link>
+          </div>
+          <div className={styles.credits}>
+            <Link to="/credits">
+              <span className={styles["credits-text-lg"]}>credits</span>
+              <span className={styles["credits-text-sm"]}>
+                <FontAwesomeIcon icon={faQuestionCircle} />
+              </span>
+            </Link>
+          </div>
+        </div>
+        {!props.isError && navRight}
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
